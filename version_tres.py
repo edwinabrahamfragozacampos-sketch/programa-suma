c01e9879ea67457d27083d8989be4039b7ccfddb
edwinabrahamfragozacampos <edwinabrahamfragozacampos@gmail.com>
2026-04-22 22:40:16 -0600

    Version tres division

---
+++ version_tres.py
@@ -0,0 +1,8 @@
+a = float(input("Ingresa el primer número: "))
+b = float(input("Ingresa el segundo número: "))
+
+if b == 0:
+    print("No se puede dividir entre 0")
+else:
+    resultado = a / b
+    print("El resultado es:", resultado)
