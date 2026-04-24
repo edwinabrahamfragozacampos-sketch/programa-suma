9577adb0374d1442bfa38dbfbdd94520f6341f3e
edwinabrahamfragozacampos <edwinabrahamfragozacampos@gmail.com>
2026-04-24 13:53:42 -0600

    Version tres ahora suma, resta y division

--- version_tres.py
+++ version_tres.py
@@ -1,8 +1,27 @@
-a = float(input("Ingresa el primer número: "))
-b = float(input("Ingresa el segundo número: "))
+print("¿Qué quieres hacer?")
+print("1. Sumar")
+print("2. Restar")
+print("3. Dividir")
 
-if b == 0:
-    print("No se puede dividir entre 0")
+opcion = input("Elige (1, 2 o 3): ")
+
+num1 = float(input("Primer número: "))
+num2 = float(input("Segundo número: "))
+
+if opcion == "1":
+    resultado = num1 + num2
+    print("Resultado:", resultado)
+
+elif opcion == "2":
+    resultado = num1 - num2
+    print("Resultado:", resultado)
+
+elif opcion == "3":
+    if num2 == 0:
+        print("No se puede dividir entre 0")
+    else:
+        resultado = num1 / num2
+        print("Resultado:", resultado)
+
 else:
-    resultado = a / b
-    print("El resultado es:", resultado)
+    print("Opción no válida")
